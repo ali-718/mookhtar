447115ea37937cdd95dd78d5eb8ad0397e4ebe5e
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-05-06 01:54:31 +0500

    week 3

--- screen/Products.js
+++ screen/Products.js
@@ -10,38 +10,65 @@ import {
 
 export default class Products extends Component {
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        isLoading: false,
-        Products: [
-          {
-            Name: "Y - Cap",
-            price: "959",
-            description:
-              "6.1 inches QHD+ Dynamic AMOLED 64-bit Octa-Core Processor 20 MP front Camera 3400 mAh battery Triple camera, 12 MP, f/1.5-2.4 + 12 MP, f/2. 4K UHD video recording at 60 fps Gorilla Glass 6 IP68 dust/water proof (up to 1.5m for 30 mins)",
-            Image:
-              "https://www.deals.mokmo.me/wp-content/uploads/2019/05/ezgif.com-webp-to-png-1.png",
-          },
-          {
-            Name: "Iphone X",
-            price: "959",
-            description:
-              "6.1 inches QHD+ Dynamic AMOLED 64-bit Octa-Core Processor 20 MP front Camera 3400 mAh battery Triple camera, 12 MP, f/1.5-2.4 + 12 MP, f/2. 4K UHD video recording at 60 fps Gorilla Glass 6 IP68 dust/water proof (up to 1.5m for 30 mins)",
-            Image:
-              "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhoneX-b.jpg",
-          },
-          {
-            Name: "Y - Cap",
-            price: "959",
-            description:
-              "6.1 inches QHD+ Dynamic AMOLED 64-bit Octa-Core Processor 20 MP front Camera 3400 mAh battery Triple camera, 12 MP, f/1.5-2.4 + 12 MP, f/2. 4K UHD video recording at 60 fps Gorilla Glass 6 IP68 dust/water proof (up to 1.5m for 30 mins)",
-            Image:
-              "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhoneX-b.jpg",
-          },
-        ],
+    // setTimeout(() => {
+    //   this.setState({
+    //     isLoading: false,
+    //     Products: [
+    //       {
+    //         Name: "Y - Cap",
+    //         price: "959",
+    //         description:
+    //           "6.1 inches QHD+ Dynamic AMOLED 64-bit Octa-Core Processor 20 MP front Camera 3400 mAh battery Triple camera, 12 MP, f/1.5-2.4 + 12 MP, f/2. 4K UHD video recording at 60 fps Gorilla Glass 6 IP68 dust/water proof (up to 1.5m for 30 mins)",
+    //         Image:
+    //           "https://www.deals.mokmo.me/wp-content/uploads/2019/05/ezgif.com-webp-to-png-1.png",
+    //       },
+    //       {
+    //         Name: "Iphone X",
+    //         price: "959",
+    //         description:
+    //           "6.1 inches QHD+ Dynamic AMOLED 64-bit Octa-Core Processor 20 MP front Camera 3400 mAh battery Triple camera, 12 MP, f/1.5-2.4 + 12 MP, f/2. 4K UHD video recording at 60 fps Gorilla Glass 6 IP68 dust/water proof (up to 1.5m for 30 mins)",
+    //         Image:
+    //           "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhoneX-b.jpg",
+    //       },
+    //       {
+    //         Name: "Y - Cap",
+    //         price: "959",
+    //         description:
+    //           "6.1 inches QHD+ Dynamic AMOLED 64-bit Octa-Core Processor 20 MP front Camera 3400 mAh battery Triple camera, 12 MP, f/1.5-2.4 + 12 MP, f/2. 4K UHD video recording at 60 fps Gorilla Glass 6 IP68 dust/water proof (up to 1.5m for 30 mins)",
+    //         Image:
+    //           "https://www.whatmobile.com.pk/admin/images/Apple/AppleiPhoneX-b.jpg",
+    //       },
+    //     ],
+    //   });
+    // }, 2000);
+
+    fetch(
+      "https://www.deals.mokmo.me/wp-json/wc/v3/products?consumer_key=ck_7f2af296ec92f1d0c8296901c1800442a5f6188a&per_page=100&page=1&consumer_secret=cs_b9663d5c3ab9ac59c1e9f19019e8aa00b7107f75"
+    )
+      .then((ali) => {
+        return ali.json();
+      })
+      .then((ali) => {
+        console.log(ali);
+        this.setState({
+          Products: ali,
+          isLoading: false,
+        });
+      })
+      .catch(() => {
+        alert("some error occoured");
       });
-    }, 2000);
   }
+
+  Name = (name) => {
+    return new Promise((resolve, reject) => {
+      if (name == "ali") {
+        resolve("Yes you are authorized");
+      } else {
+        reject("Unauthorized");
+      }
+    });
+  };
 
   state = {
     Products: [],
@@ -81,7 +108,7 @@ export default class Products extends Component {
                   >
                     <Image
                       style={{ width: "100%", height: 200, marginTop: 30 }}
-                      source={{ uri: item.Image }}
+                      source={{ uri: item.images[0].src }}
                     />
 
                     <Text
@@ -92,7 +119,7 @@ export default class Products extends Component {
                         marginBottom: 20,
                       }}
                     >
-                      {item.Name}
+                      {item.name}
                     </Text>
 
                     <View style={{ flexDirection: "row" }}>
@@ -129,11 +156,20 @@ export default class Products extends Component {
                       }}
                     >
                       <TouchableOpacity
-                        onPress={() =>
-                          this.props.navigation.navigate("Details", {
-                            product: item,
-                          })
-                        }
+                        // onPress={() =>
+                        //   this.props.navigation.navigate("Details", {
+                        //     product: item,
+                        //   })
+                        // }
+                        onPress={() => {
+                          this.Name("mookhtar")
+                            .then((res) => {
+                              alert(res);
+                            })
+                            .catch((error) => {
+                              alert(error);
+                            });
+                        }}
                         style={{
                           width: "40%",
                           height: 50,
