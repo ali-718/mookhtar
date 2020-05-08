8a9d832b0b589c98bdbc529a1daa50f354128f4f
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2020-05-09 02:23:31 +0500

    week 4

--- screen/Login.js
+++ screen/Login.js
@@ -1,10 +1,56 @@
 import React, { Component } from "react";
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 export default class Login extends Component {
   state = {
     Email: "",
     Password: "",
+  };
+
+  // Login = () => {
+  //   if (this.state.Email.trim() == "" || this.state.Password.trim() == "") {
+  //     alert("Please fill all fields");
+  //     return null;
+  //   }
+  //   alert("you are logged in");
+  // };
+
+  Login = () => {
+    if (this.state.Email.trim() == "") {
+      alert("email required");
+      return null;
+    }
+    if (this.state.Password.trim() == "") {
+      alert("password required");
+      return null;
+    }
+
+    fetch(
+      "https://www.deals.mokmo.me/wp-json/api/flutter_user/generate_auth_cookie/?consumer_key=ck_7f2af296ec92f1d0c8296901c1800442a5f6188a&consumer_secret=cs_b9663d5c3ab9ac59c1e9f19019e8aa00b7107f75 ",
+      {
+        body: JSON.stringify({
+          username: this.state.Email,
+          password: this.state.Password,
+        }),
+        headers: {
+          "Content-Type": "application/json;",
+        },
+        method: "POST",
+      }
+    )
+      .then((res) => {
+        if (res.status != 200) {
+          alert("Some error occoured");
+          return null;
+        }
+        return res.json();
+      })
+      .then((res) => console.log(res))
+      .catch((e) => {
+        console.log(e);
+      });
+
+    // alert("you are logged in");
   };
 
   render() {
@@ -36,10 +82,12 @@ export default class Login extends Component {
               Email: value,
             });
           }}
+          autoCapitalize="none"
           keyboardType="email-address"
         />
 
         <TextInput
+          autoCapitalize="none"
           placeholder="Password"
           style={{
             width: "90%",
@@ -49,6 +97,7 @@ export default class Login extends Component {
             borderWidth: 1,
             paddingLeft: 10,
             borderRadius: 10,
+            marginTop: 20,
           }}
           placeholderTextColor="gray"
           value={this.state.Password}
@@ -61,7 +110,7 @@ export default class Login extends Component {
           secureTextEntry
         />
 
-        <TextInput
+        {/* <TextInput
           placeholder="Mobile No."
           style={{
             width: "90%",
@@ -71,6 +120,7 @@ export default class Login extends Component {
             borderWidth: 1,
             paddingLeft: 10,
             borderRadius: 10,
+            marginTop: 20,
           }}
           placeholderTextColor="gray"
           value={this.state.Password}
@@ -82,10 +132,21 @@ export default class Login extends Component {
           }}
           secureTextEntry
           keyboardType="numeric"
-        />
-
-        <Text>{this.state.Email}</Text>
-        <Text>{this.state.Password}</Text>
+        /> */}
+        <TouchableOpacity
+          style={{
+            width: "90%",
+            padding: 15,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "tomato",
+            marginTop: 20,
+            borderRadius: 10,
+          }}
+          onPress={() => this.Login()}
+        >
+          <Text style={{ color: "white" }}>Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
